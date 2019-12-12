49222189eda79663c2461a01ebdded3e5c128e5d
Jomir Uddin <juddin@nyc.yearup.org>
2019-12-12 14:34:00 -0500

    created images with buttons

---
+++ classes.rb
@@ -0,0 +1,25 @@
+class Cookie
+    attr_accessor :name
+    def initialize(name, price)
+    @name = name
+    @price = price
+    end
+end
+
+class Cake
+    attr_accessor :name
+    def initialize(name, price)
+    @name = name
+    @price = price
+    end
+end
+
+class Muffin
+    attr_accessor :name
+    def initialize(name, price)
+    @name = name
+    @price = price
+    end
+end
+
+chocolate = 
--- server.rb
+++ server.rb
@@ -3,3 +3,15 @@ require 'sinatra'
 get '/' do
     erb :index
 end
+
+get '/cookies' do
+    erb :cookies
+end
+
+get '/cake' do
+    erb :cake
+end
+
+get '/muffins' do
+    erb :muffins
+end
